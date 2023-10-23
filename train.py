78790e2211bad740f68f6ee8ae8f9afbb328b921
BarkeH <82430702+BarkeH@users.noreply.github.com>
2023-10-23 20:50:05 +1100

    Update train.py

--- train.py
+++ train.py
@@ -3,27 +3,29 @@ import face_recognition
 import cv2
 import pickle
 import os
-filePaths = "dataset/"
 
-imagePaths = list(paths.list_images(filePaths))
-print(imagePaths)
+def retrain():
+    filePaths = "dataset/"
 
-names = []
-encodings = []
-for (i,imagePath) in enumerate(imagePaths):
-    print("Training image " + str(i) + "/" + str(len(imagePaths))) 
-    name = imagePath.split(os.path.sep)[-2]
-    image = cv2.imread(imagePath)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-    face_locations = face_recognition.face_locations(image, model="hog")
-    face_encodings = face_recognition.face_encodings(image, face_locations)
+    imagePaths = list(paths.list_images(filePaths))
+    print(imagePaths)
 
-    for encoding in face_encodings:
-        names.append(name)
-        encodings.append(encoding)
+    names = []
+    encodings = []
+    for (i,imagePath) in enumerate(imagePaths):
+        print("Training image " + str(i) + "/" + str(len(imagePaths)))
+        name = imagePath.split(os.path.sep)[-2]
+        image = cv2.imread(imagePath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-print("Dumping to file")
-data = {"encodings": encodings, "name": names}
-f = open("encodings.pickle", "wb")
-f.write(pickle.dumps(data))
+        face_locations = face_recognition.face_locations(image, model="hog")
+        face_encodings = face_recognition.face_encodings(image, face_locations)
+
+        for encoding in face_encodings:
+            names.append(name)
+            encodings.append(encoding)
+
+    print("Dumping to file")
+    data = {"encodings": encodings, "name": names}
+    f = open("encodings.pickle", "wb")
+    f.write(pickle.dumps(data))
